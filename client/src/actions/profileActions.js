b221f7a18eef90dc910636a5f104946c2e4a1ead
Gemju <gemju.s1@gmail.com>
2018-09-27 10:57:35 +1000

    Display Experience and Education on dashboard complete.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -80,6 +80,41 @@ export const addEducation = (eduData, history) => dispatch => {
     );
 };
 
+//Delete Experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Delete Education
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 //Delete account And profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can not be undone")) {
