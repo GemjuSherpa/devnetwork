3a4656c5b8d21924728eae67b2cabc059359c364
Gemju <gemju.s1@gmail.com>
2018-10-05 10:35:11 +1000

    Posts and Comments section: add/delete posts and comments, like and dislike the posts.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,6 +17,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/feed">
+            Post Feed
+          </Link>
+        </li>
+        <li className="nav-item">
           <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
@@ -59,7 +64,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            DevConnector
+            ProDevs
           </Link>
           <button
             className="navbar-toggler"
